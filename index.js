c63770d35ad85cc68588716b061dbfc783bd335e
kpodjison <jeevista@gmail.com>
2022-10-29 02:31:31 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+    //owl carousel 
+    $("#banner_area .owl-carousel").owlCarousel({
+        dots:true,
+        items:1,
+        loop:true,
+        autoplay:true,
+        autoplayTimeout:4000,
+        autoplayHoverPause:true
+
+    });
+});
